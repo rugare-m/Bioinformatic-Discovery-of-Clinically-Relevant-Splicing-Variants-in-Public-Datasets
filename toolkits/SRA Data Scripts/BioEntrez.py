fff610a98ff8a6c099383412834a5b844206f2cb
Rugare Maruzani <88198662+rugare-m@users.noreply.github.com>
2023-06-14 11:09:25 +0100

    Update BioEntrez.py
    clean up this script to ignore recount3 things

--- toolkits/SRA Data Scripts/BioEntrez.py
+++ toolkits/SRA Data Scripts/BioEntrez.py
@@ -7,17 +7,18 @@ lists = []
 
 runcounter = 0
 
-#doesnt change, this is tissue information from metasra
-pd_tissues = pd.read_csv('/home/rmaruzani/storage/vus/toolkits/sra/merged_metasra.csv')
+#merged_metasra.csv contains a df of studies, study ids & tissue search terms used in metaSRA
+pd_tissues = pd.read_csv('/home/rmaruzani/storage/hydra2/vus/toolkits/sra/metarecount/merged_metasra.csv')
 dictionary = dict(zip(pd_tissues['study_id'], pd_tissues['tissue search']))
 
-#take recount3-metasra Study ID instersect, and output "Study", "Run ID", "Consent", "Size (MB)", "Organism", "Tissue" csv and txt files
-#human only, no fibroblast
-with open('/home/rmaruzani/storage/vus/toolkits/sra/recount3_studies.txt') as f:
+#open a list of study ids - acquired from from merged_metasra.csv, column one
+with open('/home/rmaruzani/storage/hydra2/vus/toolkits/sra/metarecount/metasra_studies.txt') as f:
     for line in f:
         
+        #limit the number of runs to 100000
         if runcounter >= 100000:
             break
+        #sleep between requests, might be unnecessary unless making too many requests
         Entrez.sleep_between_tries = 35
         
         handle = Entrez.esearch(db="sra", term=line, retmax=10000)
@@ -51,20 +52,28 @@ with open('/home/rmaruzani/storage/vus/toolkits/sra/recount3_studies.txt') as f:
                     item.append(dictionary[key])
                     #print(item)
 
-        
+        #append to lists
         for map in mapped:
             if runcounter >= 100000:
                 break
             lists.append(map)
             runcounter += 1
-            print (runcounter)
-            print(map)
+            #print (runcounter)
+            #print(map)
         
 
 #print (lists)
         
 df = pd.DataFrame(lists, columns =["Study", "Run ID", "Consent", "Size (MB)", "Organism", "Tissue"])
+
+#discard fibroblast samples
 nofb_df = df[df['Tissue']!="fibroblast"]
+
+#replace subcutaneous adipose tissue with adipose
 nofb_df["Tissue"] = nofb_df["Tissue"].replace(to_replace="subcutaneous adipose tissue", value="adipose")
+
+#human runs only
 homo_df = nofb_df[nofb_df['Organism']=="Homo sapiens"]
-homo_df.to_csv('tissue_recount3_x_metasra.csv', encoding='utf-8', index=False) 
+
+#export the final csv
+homo_df.to_csv('tissue_metasra.csv', encoding='utf-8', index=False) 
