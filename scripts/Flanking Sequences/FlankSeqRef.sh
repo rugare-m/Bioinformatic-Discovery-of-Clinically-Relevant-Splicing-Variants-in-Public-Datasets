e2973adc46515949495b98665e60ef7b1339fdd9
Rugare Maruzani <88198662+rugare-m@users.noreply.github.com>
2023-06-16 15:51:39 +0100

    Update FlankSeqRef.sh

--- scripts/Flanking Sequences/FlankSeqRef.sh
+++ scripts/Flanking Sequences/FlankSeqRef.sh
@@ -1,1 +1,1 @@
-python flankseq_optimised2.py -v snps.clinvar.filter.vcf -r hg38.fa -o snp.ref.clinvar.fasta -f 15 -s "REF"
+python FlankSeq.py -v snps.clinvar.filter.vcf -r hg38.fa -o snp.ref.clinvar.fasta -f 15 -s "REF"
