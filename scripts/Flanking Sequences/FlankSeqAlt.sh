5851e6ae9361866ebf3243c5c8f3cddd5b9b54e1
Rugare Maruzani <88198662+rugare-m@users.noreply.github.com>
2023-06-16 15:51:52 +0100

    Update FlankSeqAlt.sh

--- scripts/Flanking Sequences/FlankSeqAlt.sh
+++ scripts/Flanking Sequences/FlankSeqAlt.sh
@@ -1,1 +1,1 @@
-python flankseq_optimised2.py -v snps.clinvar.filter.vcf -r hg38.fa -o snp.alt.clinvar.fasta -f 15 -s "ALT"
+python FlankSeq.py -v snps.clinvar.filter.vcf -r hg38.fa -o snp.alt.clinvar.fasta -f 15 -s "ALT"
