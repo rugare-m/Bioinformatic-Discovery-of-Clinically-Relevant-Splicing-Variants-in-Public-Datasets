997a67a9848db9371823148dfc8cd86ee854d246
Rugare Maruzani <88198662+rugare-m@users.noreply.github.com>
2023-06-16 15:08:18 +0100

    Update Presence.py

--- scripts/Bifrost/Presence.py
+++ scripts/Bifrost/Presence.py
@@ -166,9 +166,9 @@ def create_dataframe(vcf_file, gtf_file, dictionary):
 
         for val in dictionary[key]:
             row = []
-            srp_ids = []
-            srp_id = val.split('-')[1].split('.')[0]
-            row.append(srp_id)
+            srr_ids = []
+            srr_id = val.split('-')[2].split('.')[0]
+            row.append(srr_id)
             row.append(gene_symbol)
             row.append(chromosome)
             row.append(gene_start)
@@ -197,89 +197,3 @@ print("Dataframes created.", flush=True)
 df_sorted_ref.to_csv('reference.csv', index=False)
 df_sorted_alt.to_csv('alternate.csv', index=False)
 
-# Run the R script
-print("Initializing R script...", flush=True)
-subprocess.call(['Rscript', 'recount_query.R'])
-print("R script completed.", flush=True)
-
-# Assign scored to variants
-# Function to read junctions from a file
-def read_junctions_from_file(filename):
-    with open(filename, 'r') as file:
-        junctions = file.readlines()
-        junctions = [coord.strip() for coord in junctions]
-    return junctions
-
-# Function to calculate the fraction of times a junction appears in a list
-def calculate_fraction(item, lst):
-    count = lst.count(item)
-    total = len(lst)
-    if total == 0:
-        return 0.0
-    return count / total
-
-#get variant ids to iterate over
-variants = set()
-csv_files = ['reference.csv', 'alternate.csv'] 
-# Iterate over each CSV file
-for csv_file in csv_files:
-    with open(csv_file, 'r') as file:
-        csv_reader = csv.DictReader(file)
-        # Iterate over each row in the CSV file
-        for row in csv_reader:
-            variant_id = row['Variant ID']
-            variants.add(variant_id)
-
-# Convert the set of unique variant IDs to a list
-
-variants = list(variants)
-#print (variants, flush=True)
-
-# Get the output files from the R script
-directory = "/home/rmaruzani/storage/hydra2/vus/toolkits/bifrost"
-output_files = glob.glob(directory + "/*.output.txt")
-
-snp_scores = []
-# Iterate over each Variant ID... 
-for variant in variants:
-    for file in output_files:
-        if variant in file:
-            reference = 'ref'+'_'+variant+'.output.txt'
-            alternate = 'alt'+'_'+variant+'.output.txt'
-            
-            # Read junctions from both reference and alternate files
-            ref_jxn = read_junctions_from_file(reference)
-            alt_jxn = read_junctions_from_file(alternate)
-
-            # Combine junctions from both files
-            all_junctions = ref_jxn + alt_jxn
-
-            # Get unique junctions    
-            unique_junctions = list(set(all_junctions))
-
-            # Calculate absolute differences for each junction  
-            differences = []
-            for jxn in unique_junctions:
-                fraction_ref = calculate_fraction(jxn, ref_jxn)
-                #print(fraction_ref, flush=True)
-                fraction_alt = calculate_fraction(jxn, alt_jxn)
-                #subtract the absolute fractions
-                difference = abs(fraction_ref - fraction_alt)
-                differences.append(difference)
-
-            # Sum up the absolute differences
-            sum_of_differences = sum(differences)
-            score = []
-            # Print the result
-            file_id = reference.split('_')[1].split('.')[0]
-            score.append(file_id)
-            score.append(sum_of_differences)
-            snp_scores.append (score)
-            #print the txt file id and the score
-            #print(file, score, flush=True)
-
-#turn snp_scores into a dataframe
-snp_scores_df = pd.DataFrame(snp_scores, columns = ['Variant ID', 'Variant Score'])
-df = snp_scores_df.drop_duplicates()
-df.to_csv('variant_scores.csv', index=False)
-print(df, flush=True)
